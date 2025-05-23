9cfcc5a6b51666be6e0c5af0635372fc6c572f75
Smitha-CN <smithamalthi@gmail.com>
2025-05-23 00:43:43 -0400

    Update main.tf
    provider "aws" {
      alias  = "us_east_1"
      region = "us-east-1"
    }
    
    provider "aws" {
      alias  = "us_west_2"
      region = "us-west-2"
    }

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 # VPCs
 data "aws_caller_identity" "current" {}
 
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us_west_2"
+  region = "us-west-2"
+}
 resource "aws_vpc" "vpc_us_east_1" {
   provider             = aws.us_east_1
   cidr_block           = var.vpc_cidr_us_east_1
