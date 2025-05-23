c792eea254f5e6b2b0ec5de19e1b3365d0952ea4
Smitha-CN <smithamalthi@gmail.com>
2025-05-23 00:57:39 -0400

    Update main.tf
    provider

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 # VPCs
 data "aws_caller_identity" "current" {}
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   alias  = "us_east_1"
   region = "us-east-1"
