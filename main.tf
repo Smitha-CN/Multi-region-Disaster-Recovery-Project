64ec12aed72eacaf628ea1afbf522d758fd40f6d
Smitha-CN <smithamalthi@gmail.com>
2025-05-23 01:06:43 -0400

    Update main.tf
    commit

--- main.tf
+++ main.tf
@@ -1,24 +1,6 @@
 # VPCs
 data "aws_caller_identity" "current" {}
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
-
-provider "aws" {
-  alias  = "us_west_2"
-  region = "us-west-2"
-}
 resource "aws_vpc" "vpc_us_east_1" {
   provider             = aws.us_east_1
   cidr_block           = var.vpc_cidr_us_east_1
