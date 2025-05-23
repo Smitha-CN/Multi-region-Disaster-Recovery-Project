5629749f957c717e557977d1561274ffa2130a49
Smitha-CN <smithamalthi@gmail.com>
2025-05-23 15:21:26 -0400

    Update main.tf
    config

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 # VPCs
 data "aws_caller_identity" "current" {}
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 resource "aws_vpc" "vpc_us_east_1" {
   provider             = aws.us_east_1
   cidr_block           = var.vpc_cidr_us_east_1
