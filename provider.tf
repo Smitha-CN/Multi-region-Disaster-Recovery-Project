24770b42ef00c5fdb656d962b5a6ec4b35827ead
Smitha-CN <smithamalthi@gmail.com>
2025-05-23 15:33:07 -0400

    Update provider.tf
    empty

--- provider.tf
+++ provider.tf
@@ -1,18 +1,1 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  required_version = ">= 1.4.0"
-}
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-provider "aws" {
-  alias  = "west"
-  region = "us-west-2"
-}
