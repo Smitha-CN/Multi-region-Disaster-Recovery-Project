1198fe2475be580276aad06dbc176c6a97369d15
Smitha-CN <smithamalthi@gmail.com>
2025-05-23 15:30:54 -0400

    Update main.tf
    multi

--- main.tf
+++ main.tf
@@ -7,18 +7,33 @@ terraform {
   }
 }
 provider "aws" {
- 
+  alias  = "us_east_1"
   region = "us-east-1"
 }
 
+provider "aws" {
+  alias  = "us_west_2"
+  region = "us-west-2"
+}
 
  resource "aws_vpc" "vpc_us_east_1" {
-  
-  cidr_block           = "10.0.0.0/16"
+  provider             = aws.us_east_1
+  cidr_block           = var.vpc_cidr_us_east_1
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
     Name = "vpc-us-east-1"
+  }
+}
+
+resource "aws_vpc" "vpc_us_west_2" {
+  provider             = aws.us_west_2
+  cidr_block           = var.vpc_cidr_us_west_2
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc-us-west-2"
   }
 }
